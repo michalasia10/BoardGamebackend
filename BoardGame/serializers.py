b0cd4d44a0a65d9c552d7c44d00f7f5df5d540ee
michalasia10 <misiek1027@o2.pl>
2020-07-31 00:41:26 +0200

    "id" in JSON for games/

--- BoardGame/serializers.py
+++ BoardGame/serializers.py
@@ -8,6 +8,7 @@ class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
         fields = (
+            'id',
             'name',
             'playersNumber',
             'imgUrl',
