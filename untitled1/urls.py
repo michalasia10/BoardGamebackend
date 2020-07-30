c5bf8d7b6faf4bf0a9f4a0fe80538efd4c465f9f
michalasia10 <misiek1027@o2.pl>
2020-07-30 23:07:42 +0200

    staticfilesv1.2

--- untitled1/urls.py
+++ untitled1/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from django.conf.urls import static
+from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
