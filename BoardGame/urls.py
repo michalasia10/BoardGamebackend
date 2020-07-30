e7aa238c2b7d4e20726e15c23b330cbcff253169
michalasia10 <misiek1027@o2.pl>
2020-07-30 22:55:40 +0200

    staticfiles

--- BoardGame/urls.py
+++ BoardGame/urls.py
@@ -1,8 +1,9 @@
 from BoardGame import views
 from django.urls import path
-
+from django.conf.urls import static
+from django.conf import settings
 urlpatterns = [
     path('categorylist/',views.CategoryList.as_view(), name =views.CategoryList.name),
     path('games/',views.GameList.as_view(), name =views.GameList.name),
     path('register/',views.UserAPIView.as_view(),name = views.UserAPIView.name),
-  ]
+  ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- untitled1/urls.py
+++ untitled1/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf.urls import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('BoardGame.urls'))
-]
+    path('', include('BoardGame.urls'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
