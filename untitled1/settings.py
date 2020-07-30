ba54ce694e28f20f70f5779a690b2a8a4e3b11e8
michalasia10 <misiek1027@o2.pl>
2020-07-30 22:43:21 +0200

    staticfiles

--- untitled1/settings.py
+++ untitled1/settings.py
@@ -150,9 +150,9 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
