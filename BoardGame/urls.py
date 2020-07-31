e721ef19553fd8e63dd9752c3655aa03c3937265
michalasia10 <57455836+michalasia10@users.noreply.github.com>
2020-07-31 09:02:46 +0200

    Update urls.py
    format_suffix

--- BoardGame/urls.py
+++ BoardGame/urls.py
@@ -2,8 +2,12 @@ from BoardGame import views
 from django.urls import path
 from django.conf.urls import static
 from django.conf import settings
+from rest_framework.urlpatterns import format_suffix_patterns
+
+
 urlpatterns = [
     path('categorylist/',views.CategoryList.as_view(), name =views.CategoryList.name),
     path('games/',views.GameList.as_view(), name =views.GameList.name),
     path('register/',views.UserAPIView.as_view(),name = views.UserAPIView.name),
   ]
+urlpatterns = format_suffix_patterns(urlpatterns)
