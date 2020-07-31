dbfdafa62e58cc7cddc0f7518be4ac3c6ac31e35
michalasia10 <57455836+michalasia10@users.noreply.github.com>
2020-07-31 08:45:54 +0200

    Update settings.py
    JSONParser

--- untitled1/settings.py
+++ untitled1/settings.py
@@ -138,6 +138,12 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         # 'rest_framework.authentication.SessionAuthentic',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    ),
    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
         # 'rest_framework.permissions.IsAuthenticated',
