71a58a30c2d09eb08ab2adcd6b8f3181e954e392
michalasia10 <misiek1027@o2.pl>
2020-08-02 20:49:57 +0200

    views.py 1.3

--- BoardGame/views.py
+++ BoardGame/views.py
@@ -27,44 +27,29 @@ class GameList(generics.ListAPIView):
     name = 'game-list'
 
 
-@parser_classes((JSONParser,FormParser, MultiPartParser))
-class UserAPIView(APIView):
-    # parser_classes = [JSONParser,]
-    name = 'register'
-    def get(self,request):
-        users = Usernames.objects.all()
-        serializer = UserSerializer(users,many=True)
-        return Response(serializer.data)
+# @parser_classes((JSONParser,FormParser, MultiPartParser))
+# class UserAPIView(APIView):
+#     # parser_classes = [JSONParser,]
+#     name = 'register'
+#     def get(self,request):
+#         users = Usernames.objects.all()
+#         serializer = UserSerializer(users,many=True)
+#         return Response(serializer.data)
+#
+#     def post(self,request,format=None):
+#         body_unicode = request.body.decode('utf-8')
+#         data = json.loads(request.body).get('username')
+#         serializer = UserSerializer(data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-    def post(self,request,format=None):
-        body_unicode = request.body.decode('utf-8')
-        data = json.loads(request.body).get('username')
-        serializer = UserSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
+class UserAPIView(generics.CreateAPIView):
+    queryset = Usernames.objects.all()
+    serializer_class = UserSerializer
+    #permission_classes = [IsAuthenticated]
 
-# class UserAPIView(GenericViewSet,  # generic view functionality
-#                      CreateModelMixin,  # handles POSTs
-#                      RetrieveModelMixin,  # handles GETs for 1 Company
-#                      UpdateModelMixin,  # handles PUTs and PATCHes
-#                      ListModelMixin):  # handles GETs for many Companies
-#     serializer_class = UserSerializer
-#     queryset = Usernames.objects.all()
-#     lookup_field = 'id'
-#
-#     def get(self, request, id=None):
-#
-#         if id:
-#             return self.retrieve(request)
-#
-#         else:
-#             return self.list(request)
-#
-#     def post(self, request):
-#         return self.create(request)
-#
-#     def put(self, request, id=None):
-#         return self.update(request, id)
+    def perform_create(self, serializer):
+        serializer.save(username=self.request.username)
