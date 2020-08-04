fb5c0beb441ff18fabf70be6832527e6221ecae6
michalasia10 <57455836+michalasia10@users.noreply.github.com>
2020-08-04 17:00:22 +0200

    Update urls.py
    debug_toolbar off

--- untitled1/urls.py
+++ untitled1/urls.py
@@ -23,5 +23,5 @@ import debug_toolbar
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('BoardGame.urls')),
-    path('__debug__',include(debug_toolbar.urls)),
+    #path('__debug__',include(debug_toolbar.urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
