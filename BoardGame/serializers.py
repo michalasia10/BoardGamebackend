031381a5ba76bf1fdf197227ab5d3d7420611a44
michalasia10 <misiek1027@o2.pl>
2020-09-29 17:56:50 +0200

    Matches serializers.py 29.09 _01

--- BoardGame/serializers.py
+++ BoardGame/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from .models import Project,Game,User,Match,Player
+from .models import Project, Game, User, Match, Player
 from rest_framework.exceptions import APIException
 from rest_framework import status
 from django.db import IntegrityError
 
-class GameSerializer(serializers.ModelSerializer):
 
+class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
         fields = (
@@ -15,31 +15,29 @@ class GameSerializer(serializers.ModelSerializer):
         )
 
 
-
 class CategorySerializer(serializers.ModelSerializer):
-    games = GameSerializer(many=True,read_only=True)
+    games = GameSerializer(many=True, read_only=True)
+
     class Meta:
         model = Project
         fields = (
-            'projectName','games',
+            'projectName', 'games',
         )
 
 
-class MatchSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Match
-        fields = (
-            'id',
-            'game',
-            'playersNumber',
-        )
-    def create(self, validated_data):
-        return Match.objects.create(**validated_data)
-
+# class MatchSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Match
+#         fields = (
+#             'id',
+#             'game',
+#             'playersNumber',
+#         )
+#     def create(self, validated_data):
+#         return Match.objects.create(**validated_data)
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-
     class Meta:
         model = User
         fields = (
@@ -49,22 +47,41 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     def create(self, validated_data):
         return User.objects.create(**validated_data)
 
+
 class Players(serializers.ModelSerializer):
     matchId = serializers.ReadOnlyField(source='room.id')
     userId = serializers.ReadOnlyField(source='playerName.id')
+
     class Meta:
         model = Player
         fields = (
-            'userId','matchId',
+            'userId', 'matchId',
         )
+
+
+class MatchSerializer(serializers.ModelSerializer):
+    players = Players(many=True)
+
+    class Meta:
+        model = Match
+        fields = (
+            'id',
+            'game',
+            'playersNumber',
+            'players',
+        )
+
+    def create(self, validated_data):
+        return Match.objects.create(**validated_data)
+
 
 class RoomSerializer(serializers.ModelSerializer):
     rooms = MatchSerializer(many=True)
+
     class Meta:
         model = Game
         fields = (
             'id',
             'name',
             'rooms',
-
         )
