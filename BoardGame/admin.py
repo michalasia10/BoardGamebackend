428394d317df0d9116146d1715bbf7f9241e45b8
michalasia10 <misiek1027@o2.pl>
2020-09-30 21:25:06 +0200

    UserAdmin 30.09 _01

--- BoardGame/admin.py
+++ BoardGame/admin.py
@@ -3,8 +3,11 @@ from BoardGame.models import Project,Game,User,Player,Match
 
 admin.site.register(Project)
 admin.site.register(Game)
-admin.site.register(User)
-# admin.site.register(Player)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','username')
+    search_fields = ('id','username')
+
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
     list_display = ('game','maxPlayers')
