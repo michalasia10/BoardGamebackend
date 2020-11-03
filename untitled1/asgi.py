94d28861594cd7af4bc44fa6259f7afc914ab07e
michalasia10 <57455836+michalasia10@users.noreply.github.com>
2020-11-03 23:45:01 +0100

    Update asgi.py

--- untitled1/asgi.py
+++ untitled1/asgi.py
@@ -16,15 +16,10 @@ from channels.routing import ProtocolTypeRouter,URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 import BoardGame.routing
+import django
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'untitled1.settings')
 django.setup()
-# application = ProtocolTypeRouter({
-#     'http':get_asgi_application(),
-#     'webscoket':AuthMiddlewareStack(
-#         URLRouter(
-#             BoardGame.routing.websocket_urlpatterns
-#         )
-#     )
-# })
 application = get_default_application()
+channel_layer = get_channel_layer()
