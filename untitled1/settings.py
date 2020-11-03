96d738ddfdfb7f24b75c736a102b7975e76cbc56
michalasia10 <57455836+michalasia10@users.noreply.github.com>
2020-11-04 00:07:50 +0100

    Update settings.py

--- untitled1/settings.py
+++ untitled1/settings.py
@@ -156,7 +156,7 @@ CHANNEL_LAYERS = {
     'default':{
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG':{
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [os.environ.get('REDIS_HOSTNAME'), 6379)],
         }
     }
 }
