18e835c17a74d23945091924d3279a63ddab63e1
michalasia10 <57455836+michalasia10@users.noreply.github.com>
2020-11-03 22:59:32 +0100

    Update asgi.py

--- untitled1/asgi.py
+++ untitled1/asgi.py
@@ -27,6 +27,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'untitled1.settings')
 
 from channels.routing import get_default_application
 import django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cfehome.settings")
 django.setup()
 application = get_default_application()
