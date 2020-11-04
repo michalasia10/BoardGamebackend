8b153b6245ad4f02ee727e7f79439ecec53eecec
michalasia10 <57455836+michalasia10@users.noreply.github.com>
2020-11-04 13:54:35 +0100

    Update settings.py

--- untitled1/settings.py
+++ untitled1/settings.py
@@ -151,6 +151,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CHANNEL_LAYERS = {
     'default':{
