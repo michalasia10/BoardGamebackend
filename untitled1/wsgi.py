0b23ad55daf9e71f2bb38593e7904457b97d52e1
michalasia10 <57455836+michalasia10@users.noreply.github.com>
2020-11-04 01:02:27 +0100

    Update wsgi.py

--- untitled1/wsgi.py
+++ untitled1/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'untitled1.settings')
-
+django.setup()
 application = get_wsgi_application()
