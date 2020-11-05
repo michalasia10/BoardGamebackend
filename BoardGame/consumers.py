24b96f77111bbbaa0c7898f67f988d4814ae66bd
michalasia10 <misiek1027@o2.pl>
2020-11-05 22:00:51 +0100

    reformat __init__ in RoomConsumer

--- BoardGame/consumers.py
+++ BoardGame/consumers.py
@@ -8,14 +8,12 @@ from django.shortcuts import get_object_or_404
 
 class RoomConsumer(AsyncWebsocketConsumer):
 
-    # def __init__(self, *args, **kwargs):
-    #     super(RoomConsumer,self).__init__(*args, **kwargs)
-    #     self.room_name = self.scope['url_route']['kwargs']['match_pk']
-    #     self.room_group_name = self.room_name
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.room_group_name = self.room_name
+        self.room_name = self.scope['url_route']['kwargs']['match_pk']
 
     async def websocket_connect(self, event):
-        self.room_name = self.scope['url_route']['kwargs']['match_pk']
-        self.room_group_name = self.room_name
         print(f"Connected, typ of connect {event}")
         match = await self.get_match(pk=self.room_name)
         # Join room group
