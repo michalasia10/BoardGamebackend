2e6b623bd721c3faac44aed814c65da0940cce3c
michalasia10 <misiek1027@o2.pl>
2020-11-05 17:15:34 +0100

    change url

--- BoardGame/routing.py
+++ BoardGame/routing.py
@@ -1,7 +1,7 @@
-from django.urls import re_path
+from django.conf.urls import url
 
 from .consumers import RoomConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/room/(?P<match_pk>[0-9]+)$', RoomConsumer,name='room')
+    url(r'ws/room/(?P<match_pk>[0-9]+)$', RoomConsumer,name='room')
 ]
