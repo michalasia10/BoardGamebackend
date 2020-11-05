14374a9084c789980bebe694a03ec3cf3f9527f5
michalasia10 <misiek1027@o2.pl>
2020-11-05 22:01:36 +0100

    reformat __init__ RoomCOnsumer

--- BoardGame/consumers.py
+++ BoardGame/consumers.py
@@ -10,8 +10,8 @@ class RoomConsumer(AsyncWebsocketConsumer):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.room_group_name = self.room_name
         self.room_name = self.scope['url_route']['kwargs']['match_pk']
+        self.room_group_name = self.room_name
 
     async def websocket_connect(self, event):
         print(f"Connected, typ of connect {event}")
