2baf8a8295e4fb9c349a702d67e17a464353caa3
michalasia10 <misiek1027@o2.pl>
2020-11-10 13:40:29 +0100

    reformat INSTALLED_APPS

--- untitled1/settings.py
+++ untitled1/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'board_game.apps.BoardgameConfig',
+
     'rest_framework',
     'corsheaders',
     'channels',
+
+    'board_game.apps.BoardgameConfig',
 
 ]
 
