fb34b64e366174be95f49e8e70bb6885d9929c33
michalasia10 <misiek1027@o2.pl>
2020-11-18 02:25:24 +0100

    test 1.3

--- board_game/consumers.py
+++ board_game/consumers.py
@@ -12,6 +12,7 @@ class RoomConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['match_pk']
         self.room_group_name = self.room_name
         print(f"Connected, typ of connect {event}")
+        print(self.room_group_name)
         match = await self.get_match(pk=self.room_name)
         # Join room group
         await self.channel_layer.group_add(
@@ -44,7 +45,7 @@ class RoomConsumer(AsyncWebsocketConsumer):
     async def newstate(self, event):
         dicta = json.loads(event['data'])
 
-        await self.send(json.dumps(dicta))
+        await self.send(text_data=json.dumps(dicta))
         print(f' Automatically updated database content {dicta}')
 
     async def message(self, event):
